a80143d9a1193b2a7705eafd246db9efe0e216f4
Logan MacAskill <logan@macaskill.com>
2022-09-01 13:02:35 -0700

    move it out

--- old_portfolio/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-
-const Footer = () => (
-  <footer className='footer'>
-    <a href='https://github.com/ImWolverine/portfolio'>Source on Github</a>
-  </footer>
-)
-
-export default Footer
