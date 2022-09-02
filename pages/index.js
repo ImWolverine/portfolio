2d510ba9a89c50f8be5df862fb49814afe338520
Logan MacAskill <logan@macaskill.com>
2022-09-02 08:25:46 -0700

    fix spelling

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ posts }) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
-            Lastest Projects
+            Latest Projects
           </h1>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
