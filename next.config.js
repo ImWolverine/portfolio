0cd80acd63ecfbae325983722deefcfb7853dac2
Logan MacAskill <logan@macaskill.com>
2022-09-01 20:58:31 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
-  style-src 'self' 'unsafe-inline' *;
+  style-src 'self' 'unsafe-inline' use.typekit.net p.typekit.net;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
